e7a5078a8edcf87ad6a67f16f608ca4a4a946636
Anastasia <aburlakova45@gmail.com>
2023-07-25 20:35:18 +0300

    функция __init__

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,16 @@
+import os
+from googleapiclient.discovery import build
 
 
 class Channel:
+    API_KEY = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
+        """Выводит в консоль информацию python -m venv venvо канале."""
         pass
