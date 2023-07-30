fe5afea29d2564152d6a46ced960c5c44ceac96d
Anastasia <aburlakova45@gmail.com>
2023-07-30 04:09:57 +0300

    добавляем атрибуты

--- src/channel.py
+++ src/channel.py
@@ -11,8 +11,16 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']
+        self.channel_descr = channel['items'][0]['snippet']['description']
+        self.url = "https://www.youtube.com/" + channel['items'][0]['snippet']['customUrl']
+        self.quantity_sub = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.quantity_all_vievs = channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию python -m venv venvо канале."""
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
