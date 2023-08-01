0c43c51b465eb84be6cfe11b0e708ac12decbcbf
Anastasia <aburlakova45@gmail.com>
2023-08-01 04:48:23 +0300

    реализация метода __gt__

--- src/channel.py
+++ src/channel.py
@@ -28,6 +28,9 @@ class Channel:
     def __sub__(self, other):
         return int(self.quantity_sub) - int(other.quantity_sub)
 
+    def __gt__(self, other):
+        return self.quantity_sub > other.quantity_sub
+
     def print_info(self) -> None:
         """Выводит в консоль информацию python -m venv venvо канале."""
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
